bab752069ba04a8973ad82e0df2ac9e6309b7324
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2023-02-12 23:02:16 +0530

    Time: 93 ms (31.54%), Space: 6.7 MB (27.25%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int recurse(int n,vector<int> &dp){
+        if(n==1 || n==0)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+        int res = INT_MAX;
+        for(int i=1;i<=n;i++){
+            res = min(res,max(i,recurse(n-i,dp)+1));
+        }
+        return dp[n]=res;
+    }
+    int twoEggDrop(int n) {
+        vector<int> dp(n+1,-1);
+        return recurse(n,dp);
+    }
+};
