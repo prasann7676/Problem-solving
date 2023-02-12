a2d03d1f420709d4c57fae426917c152b03cd4a7
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2023-02-12 19:09:55 +0530

    Time: 4 ms (65.17%), Space: 7.7 MB (26.78%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -2,39 +2,24 @@ class Solution {
 public:
     int countSubstrings(string s, string t) {
         int n = s.size(), m = t.size();
-        unordered_map<string,int> mp,dp;
-        for(int i=0;i<m;i++){
-            string tmp = "";
-            for(int j=i;j<m;j++){
-                tmp += t[j];
-                mp[tmp]++;
+        vector<vector<int>> pre(n+2,vector<int>(m+2,0)),suf(n+2,vector<int>(m+2,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1])
+                    pre[i][j]=pre[i-1][j-1]+1;
+            }
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=m;j>=1;j--){
+                if(s[i-1]==t[j-1])
+                    suf[i][j]=suf[i+1][j+1]+1;
             }
         }
         int ans = 0;
-        for(int i=0;i<n;i++){
-            string tmp = "";
-            for(int j=i;j<n;j++){
-                tmp += s[j];
-                if(dp.find(tmp) != dp.end()){
-                    ans += dp[tmp];
-                    continue;
-                }
-                int res = 0;
-                for(int k=0;k<(int)tmp.size();k++){
-                    char org = tmp[k];
-                    for(char c='a';c<='z';c++){
-                        if(org==c)
-                            continue;
-                        tmp[k]=c;
-                        // cout<<tmp<< " "<<i<< " "<<j<<" "<<k<< "\n";
-                        if(mp.find(tmp) != mp.end()){
-                            res+=mp[tmp];
-                        }
-                    }
-                    tmp[k]=org;
-                }
-                ans += res;
-                dp[tmp]=res;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]!=t[j-1])
+                    ans += (pre[i-1][j-1]+1)*(suf[i+1][j+1]+1);
             }
         }
         return ans;
