b12e37a2f5919eeb2cdb19cda7bddc9a972cc470
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2023-02-14 18:09:11 +0530

    Time: 29 ms (85.30%), Space: 6.7 MB (96.29%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> cur(m+1), pre(m+1);
         int sum=0;
-        for(int i=1;i<=n;i++){
-            sum += (int)s1[i-1];
-            dp[i][0] = sum;
-        }
-        sum=0;
         for(int i=1;i<=m;i++){
             sum += (int)s2[i-1];
-            dp[0][i] = sum;
+            pre[i] = sum;
         }
+        sum=0;
         for(int i=1;i<=n;i++){
+            sum += (int)s1[i-1];
+            cur[0] = sum;
             for(int j=1;j<=m;j++){
                 if(s1[i-1]==s2[j-1])
-                    dp[i][j]=dp[i-1][j-1];
+                    cur[j]=pre[j-1];
                 else
-                    dp[i][j]=min(dp[i-1][j]+(int)s1[i-1],dp[i][j-1]+(int)s2[j-1]);
+                    cur[j]=min(pre[j]+(int)s1[i-1],cur[j-1]+(int)s2[j-1]);
             }
+            pre=cur;
         }
-        return dp[n][m];
+        return pre[m];
     }
 };
