e69dbef6d0a87723f38c7f64ebac60eeeb229f03
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2023-02-19 13:46:36 +0530

    Time: 3 ms (72.30%), Space: 8.4 MB (14.13%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
+    static bool comp(vector<int> &a, vector<int> &b){
+        if(a[1]==b[1])
+            return a[0]<b[0];
+        return a[1]<b[1];
+    }
     int videoStitching(vector<vector<int>>& clips, int T) {
-        sort(begin(clips), end(clips));
-        int res = 0;
-        for (auto i = 0, st = 0, end = 0; st < T; st = end, ++res) {
-            for (; i < clips.size() && clips[i][0] <= st; ++i)
-                end = max(end, clips[i][1]);
-            if (st == end) return -1;
+        int n = clips.size();
+        sort(clips.begin(),clips.end(),comp);
+        vector<vector<int>> dp(T+1,vector<int>(n+1,1e9));
+        for(int i=0;i<=n;i++)
+            dp[0][i]=0;
+        for(int i=1;i<=T;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j] = dp[i][j-1];
+                if(clips[j-1][1]>=i)
+                    dp[i][j] = min(dp[i][j],dp[min(T,clips[j-1][0])][j-1]+1);
+            }
         }
-        return res;
+        // for(int i=1;i<=n;i++)
+        //     cout<<i<< " "<<dp[T][i]<< "\n";
+        if(dp[T][n]==1e9)
+            return -1;
+        return dp[T][n];
     }
 };
