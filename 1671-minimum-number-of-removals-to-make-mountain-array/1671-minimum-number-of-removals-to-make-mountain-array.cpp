4de452b11f4224998a704b5e42e9d6cce3284d60
Prasann Kumar Gupta <56448719+prasann7676@users.noreply.github.com>
2024-10-30 15:42:20 +0530

    Time: 0 ms (100.00%), Space: 14.7 MB (83.50%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -2,24 +2,34 @@ class Solution {
 public:
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size(),ans = INT_MAX;
-        vector<int> pre(n,1), suf(n,1);
+        vector<int> left_inc, right_inc, pre_inc(n,1);
+        left_inc.push_back(nums[0]); right_inc.push_back(nums[n-1]);
         for(int i=1;i<n;i++){
-            for(int j=i-1;j>=0;j--)
-                if(nums[j]<nums[i])
-                    pre[i]=max(pre[i],pre[j]+1);
+            if(nums[i]>left_inc.back()){
+                left_inc.push_back(nums[i]);
+                pre_inc[i] = (int)left_inc.size();
+            }
+            else{
+                int ind = lower_bound(left_inc.begin(), left_inc.end(), nums[i])-left_inc.begin();
+                left_inc[ind]=nums[i];
+                pre_inc[i]=ind+1;
+            }
         }
         for(int i=n-2;i>=0;i--){
-            for(int j=i+1;j<n;j++){
-                if(nums[j]<nums[i])
-                    suf[i]=max(suf[i],suf[j]+1);
+            int suf_inc = 1;
+            if(nums[i]>right_inc.back()){
+                right_inc.push_back(nums[i]);
+                suf_inc = (int)right_inc.size();
             }
-            if(i>0){
-                int num_left = i+1, num_right = (n-i);
-                int right_void = num_right-suf[i];
-                int left_void = num_left-pre[i];
-                // cout<<i<< " "<<left_void<< " "<<right_void<< " "<<pre[i]<< " "<<suf[i]<< "\n";
-                if(pre[i]>1 && suf[i]>1)
-                    ans = min(ans, right_void+left_void);
+            else{
+                int ind = lower_bound(right_inc.begin(),right_inc.end(),nums[i])-right_inc.begin();
+                right_inc[ind]=nums[i];
+                suf_inc = ind+1;
+            }
+            if(pre_inc[i]>1 && suf_inc>1){
+                int left_void = (i+1)-pre_inc[i];
+                int right_void = (n-i)-suf_inc;
+                ans = min(ans, left_void + right_void);
             }
         }
         return ans;
